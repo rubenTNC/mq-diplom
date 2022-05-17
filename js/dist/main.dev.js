017b20e84c7fa5b75a26d26c77da7b8374d3fa79
rubenTNC <102420221+rubenTNC@users.noreply.github.com>
2022-05-17 21:38:44 +0300

    Add files via upload

--- js/dist/main.dev.js
+++ js/dist/main.dev.js
@@ -1,8 +1,16 @@
 "use strict";
 
-var menu = document.getElementById("menu");
-
 function mobile() {
-  menu.classList.toggle("menu-open");
+  document.getElementById("menu").classList.toggle("menu-open");
 }
+
+var popup = document.querySelector(".popup");
+window.onload = setTimeout(function () {
+  return popup.classList.toggle("open");
+}, 10000);
+var close = document.querySelector(".close");
+
+close.onclick = function () {
+  popup.classList.remove("open");
+};
 //# sourceMappingURL=main.dev.js.map
--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-let menu = document.getElementById("menu");
 function mobile() {
-    menu.classList.toggle("menu-open")
+    document.getElementById("menu").classList.toggle("menu-open");
 }
+
+let popup = document.querySelector(".popup");
+window.onload =  setTimeout(() => popup.classList.toggle("open"), 10000);
+
+let close = document.querySelector(".close");
+
+close.onclick = function() {
+    popup.classList.remove("open")
+}
+
